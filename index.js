49340089860a3a589c26cc80eee8058273c27d01
2Tucho <albergromero@gmail.com>
2024-03-02 13:42:35 +0100

    add/function cardTemplate and fetch of countries

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-const cardTemplate = function (/* You can pass the data here*/) {
+//Localización del id de cada bandera: [i].cca3
+//Localización de la bandera: [i].flags.png
+//Localización del alt de la imagen: [i].flags.alt
+//Localización del nombre: [i].name.common
+
+function cardTemplate (eachCountry) {
+  if (eachCountry.flags.alt === undefined) eachCountry.flags.alt = "This country's flag isn't explained in countries.api"
   return `<div class="card">
-              <img id="flag-image" src="ADD THE IMAGE LINK HERE" alt="flag" />
-              <h1 class="center">ADD COUNTRY NAME HERE</h1>
+              <img id="${eachCountry.cca3}" src="${eachCountry.flags.png}" alt="${eachCountry.flags.alt}" />
+              <h1 class="center">${eachCountry.name.common}</h1>
             </div>`;
 };
 
 const countriesNode = document.getElementById("countries");
 
-fetch(/* Need the provide API URL to get all countries */)
-  .then(function (response) {
-    // fetch() returns a promise containing the response (a Response object).
-    // This is just an HTTP response, not the actual JSON. 
-    // To extract the JSON body content from the response, 
-    // we use the json() method and pass it into the next .then()
-  })
+fetch("https://restcountries.com/v3.1/all")
+  .then(res => res.json())
   .then(function (countries) {
     // Here is where you'll need to add into the DOM all the countries received from API 
-
+      countries.sort((a, b) => a.name.common.localeCompare(b.name.common))
+      for (let i = 0; i < countries.length; i++) {
+        countriesNode.innerHTML += cardTemplate(countries[i])
+      }
     // 1 - We will need to iterate the countries variable with a loop
     // 2 - You can use the cardTemplate() function to create a div with a class card already styled
     // 💡 you can use countriesNode variable to add elements
   });
+
+  //Funciona pero sin organizar alfabeticamente: 
+  /* for (let i = 0; i < countries.length; i++) {
+    //Ordenar el array de objetos por atributo: computadoras.sort((a, b) => a.name.common.localeCompare(b.name.common))
+    countriesNode.innerHTML += cardTemplate(countries[i])
+  } */
+
+  
+
+  //Para el premium, ya están los paises organizados por continentes, se accede por [i].continents
